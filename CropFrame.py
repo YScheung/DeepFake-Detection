2d417bf9ba9b03530b7c90d859a15755a81374d9
YScheung <72407100+YScheung@users.noreply.github.com>
2021-07-11 19:17:18 +0800

    Added comments

--- CropFrame.py
+++ CropFrame.py
@@ -1,3 +1,5 @@
+# Crop frame images from youtube videos
+
 import cv2
 import pytube
 import os
@@ -6,14 +8,14 @@ import pytube.exceptions
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 linklist = []
-with open('urls_fake.csv') as f:
+with open('urls_fake.csv') as f:  #csv file that contain links of youtube videos 
     data = csv.reader(f)
     for row in data:
         print(row[0])
         linklist.append(row[0])
 
 fcount=0
-count=367800
+count=0
 for url in linklist:
     try:
         youtube = pytube.YouTube(url)
@@ -24,9 +26,9 @@ for url in linklist:
         success,image = vidcap.read()
         while success:
             success,image = vidcap.read()
-            if (count % 200 == 0):
+            if (count % 200 == 0):  
               cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
             count += 1
     except pytube.exceptions.VideoUnavailable:
-        print("Failed")
+        print("Video unavailable")
     fcount += 1
